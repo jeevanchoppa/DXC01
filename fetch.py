be85725e41a8ecf7f5354a72cc8acf70eba99355
jeevan <jsatyanaraya@dxc.com>
2022-09-14 18:02:52 +0530

    e

--- fetch.py
+++ fetch.py
@@ -1,17 +1,13 @@
-import sqlite3
+def fetch ():
+    import sqlite3
+    con = sqlite3.connect('StudDetails01.db')
+    cur = con.cursor()
+    data = cur.execute('''select * from StudDetails01''')
+    for row in data:
+        RollNo = row[0]
+        StudName = row[1]
+        PhyMarks = row[2]
+        CheMarks = row[3]
+        MathMarks = row[4]
 
-con = sqlite3.connect('StudDetails01.db')
-
-cur = con.cursor()
-
-data = cur.execute('''select * from StudDetails01''')
-
-for row in data:
-    rollNo = row[0]
-    studName = row[1]
-    phyMarks = row[2]
-    cheMarks = row[3]
-    mathMarks = row[4]
-    print(str(rollNo) + "   " + studName + "   " + str(phyMarks) + "   " + str(cheMarks)+"   " + str(mathMarks))
-
-con.close()
+        print(str(RollNo) + "  ,  " + StudName + "  ,  " + str(PhyMarks) + "  ,  " + str(CheMarks) + "  ,  " + str(MathMarks))
